a77880dbe49950cd54cb1ee9e428e239a0c20b6b
Elena Olompieva <e.olompieva@yandex.ru>
2020-07-03 17:37:29 +0500

    create and update procedure

---
+++ Procedure/Add_Roles.sql
@@ -0,0 +1,9 @@
+﻿USE Enterprise_Management;
+
+GO
+CREATE PROCEDURE Add_Roles (@Employee_id INT, @Role_id INT, @Active_from DATE)
+AS
+BEGIN
+  INSERT INTO Employees_Roles_links (Employee_id, Role_id, Active_From, Active_To)
+    VALUES (@Employee_id, @Role_id, @Active_from, NULL)
+END
---
+++ Procedure/Change_Role.sql
@@ -0,0 +1,13 @@
+﻿USE Enterprise_Management;
+
+GO
+CREATE PROCEDURE Change_Role (@Employee_id INT, @New_Role_id INT, @Date DATE)
+AS
+BEGIN
+  UPDATE Employees_Roles_links
+  SET Active_To = @Date
+  WHERE Employee_id = @Employee_id
+
+  INSERT INTO Employees_Roles_links (Employee_id, Role_id, Active_From, Active_To)
+    VALUES (@Employee_id, @New_Role_id, @Date, NULL)
+END
