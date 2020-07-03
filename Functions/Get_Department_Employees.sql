e20b8d1034c0938d57d69d8ed8a871f4b6e73fe6
Elena Olompieva <e.olompieva@yandex.ru>
2020-07-03 16:32:20 +0500

    create function and procedure

---
+++ Functions/Get_Department_Employees.sql
@@ -0,0 +1,23 @@
+﻿USE Enterprise_Management;
+
+GO
+CREATE FUNCTION dbo.Get_Department_Employees (@Department_Name NVARCHAR(100))
+RETURNS TABLE
+AS
+  RETURN (SELECT
+      e.Last_Name
+     ,e.First_Name
+     ,e.Middle_Name
+     ,r.Role_Name
+     ,d.Department_Name
+    FROM Employees e
+    JOIN Employees_Roles_links erl
+      ON e.Employee_id = erl.Employee_id
+    JOIN Roles_Departments_links rdl
+      ON erl.Role_id = rdl.Role_id
+    JOIN Departments d
+      ON rdl.Department_id = d.Department_id
+    JOIN Roles r
+      ON erl.Role_id = r.Role_id
+    WHERE d.Department_Name = @Department_Name
+    AND erl.Active_To IS NULL)
---
+++ Procedure/Delete_Roles.sql
@@ -0,0 +1,11 @@
+﻿USE Enterprise_Management;
+
+GO
+CREATE PROCEDURE Delete_Roles (@Employee_id INT, @Role_id INT, @Active_to DATE)
+AS
+BEGIN
+  UPDATE Employees_Roles_links
+  SET Active_To = @Active_to
+  WHERE Employee_id = @Employee_id
+  AND Role_id = @Role_id
+END
