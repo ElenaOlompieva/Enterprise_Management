0c7a4b9428768c7581f157dd4249230f6613e177
Elena Olompieva <e.olompieva@yandex.ru>
2020-07-04 15:54:54 +0500

    add schema and keys

--- create db and tables.sql
+++ create db and tables.sql
@@ -1,33 +1,59 @@
 ﻿CREATE DATABASE Enterprise_Management;  
 GO
 
-CREATE TABLE Employees
-   (Employee_id INT IDENTITY PRIMARY KEY NOT NULL,  
-    Last_Name NVARCHAR (50) NOT NULL,  
-    First_Name NVARCHAR (50) NOT NULL, 
-    Middle_Name NVARCHAR (50)); 
+CREATE TABLE Enterprise_Management.dbo.Employees (
+  Employee_id INT IDENTITY
+ ,Last_Name NVARCHAR(50) NOT NULL
+ ,First_Name NVARCHAR(50) NOT NULL
+ ,Middle_Name NVARCHAR(50) NULL
+ ,PRIMARY KEY CLUSTERED (Employee_id)
+) ON [PRIMARY]
 GO
 
-CREATE TABLE Roles  
-   (Role_id INT IDENTITY PRIMARY KEY NOT NULL,  
-    Role_Name NVARCHAR (100) NOT NULL); 
+CREATE TABLE Enterprise_Management.dbo.Roles (
+  Role_id INT IDENTITY
+ ,Role_Name NVARCHAR(100) NOT NULL
+ ,PRIMARY KEY CLUSTERED (Role_id)
+) ON [PRIMARY]
 GO
 
-CREATE TABLE Departments  
-   (Department_id INT IDENTITY PRIMARY KEY NOT NULL,  
-    Department_Name NVARCHAR (100) NOT NULL); 
+CREATE TABLE Enterprise_Management.dbo.Departments (
+  Department_id INT IDENTITY
+ ,Department_Name NVARCHAR(100) NOT NULL
+ ,PRIMARY KEY CLUSTERED (Department_id)
+) ON [PRIMARY]
 GO
 
-CREATE TABLE Employees_Roles_links  
-   (Link_id INT IDENTITY PRIMARY KEY NOT NULL,
-    Employee_id INT NOT NULL,
-    Role_id INT NOT NULL,
-    Active_From DATE NOT NULL,
-    Active_To DATE);  
+CREATE TABLE Enterprise_Management.dbo.Employees_Roles_links (
+  Link_id INT IDENTITY
+ ,Employee_id INT NOT NULL
+ ,Role_id INT NOT NULL
+ ,Active_From DATE NOT NULL
+ ,Active_To DATE NULL
+ ,CONSTRAINT PK_Employees_Roles_links_Link_id PRIMARY KEY CLUSTERED (Link_id)
+) ON [PRIMARY]
 GO
 
-CREATE TABLE Roles_Departments_links  
-   (Link_id INT IDENTITY PRIMARY KEY NOT NULL,
-    Role_id INT NOT NULL,
-    Department_id INT NOT NULL);  
+ALTER TABLE Enterprise_Management.dbo.Employees_Roles_links
+ADD CONSTRAINT FK_Employees_Roles_links FOREIGN KEY (Employee_id) REFERENCES dbo.Employees (Employee_id)
+GO
+
+ALTER TABLE Enterprise_Management.dbo.Employees_Roles_links
+ADD CONSTRAINT FK_Employees_Roles_links2 FOREIGN KEY (Role_id) REFERENCES dbo.Roles (Role_id)
+GO
+
+CREATE TABLE Enterprise_Management.dbo.Roles_Departments_links (
+  Link_id INT IDENTITY
+ ,Role_id INT NOT NULL
+ ,Department_id INT NOT NULL
+ ,CONSTRAINT PK_Roles_Departments_links_Link_id PRIMARY KEY CLUSTERED (Link_id)
+) ON [PRIMARY]
+GO
+
+ALTER TABLE Enterprise_Management.dbo.Roles_Departments_links
+ADD CONSTRAINT FK_Roles_Departments_links FOREIGN KEY (Role_id) REFERENCES dbo.Roles (Role_id)
+GO
+
+ALTER TABLE Enterprise_Management.dbo.Roles_Departments_links
+ADD CONSTRAINT FK_Roles_Departments_links2 FOREIGN KEY (Department_id) REFERENCES dbo.Departments (Department_id)
 GO
